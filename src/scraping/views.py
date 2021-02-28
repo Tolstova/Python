2c2bad7af0fcac120d75d1c20e8efa3dd9f6e35b
Tolstova <ol.vl.tolstova@gmail.com>
2021-02-28 19:12:04 +0300

    6d_filters_with_form

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -4,6 +4,18 @@ from .models import Vacancy
 
 # Create your views here.
 def home_view(request):
-    print(request.POST)
-    qs= Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list':qs})
+    # print(request.GET)
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
+
+   # qs = Vacancy.objects.filter(**_filter)
+    qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs})
+
